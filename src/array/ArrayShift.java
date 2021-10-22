aeb9b10095617397e0b11ac7083811f7971ea6e2
srinivasbe-07 <ksrinivaslu25@gmail.com>
2021-10-22 09:07:38 +0530

    Modified array shift

--- src/array/ArrayShift.java
+++ src/array/ArrayShift.java
@@ -4,26 +4,57 @@ import java.util.Arrays;
 
 public class ArrayShift {
 
-	private int[] reverseArray(int[] arr, int i , int j) {
-		while(i < j) {
-			arr[i] = arr[i] + arr[j];
-			arr[j] = arr[i] - arr[j];
-			arr[i] = arr[i] - arr[j];
-			i++;
-			j--;
+	// [1, 2, 3, 4, 5, 6, 7]
+		// shift from the beginning and put last value to initial
+		// [7, 1, 2, 3, 4, 5, 6]
+		// [6, 7, 1, 2, 3, 4, 5]
+
+
+		public static int[] rotateArray1(int[] arr, int d) {
+
+			// with out extra memory and compexity O(m*n)
+			int len = arr.length;
+			for (int i = 0; i < d; i++) {
+				int curr = arr[0];
+				for (int j = 0; j < len - 1; j++) {
+					int temp = arr[j+1];
+					arr[j + 1] = curr;
+					curr  = temp;
+
+				}
+				arr[0] = curr;
+			}
+
+			return arr;
 		}
-		return arr;
-	}
-	public int[] shiftArray(int[] arr, int n) {
+
+		// [1, 2, 3, 4, 5, 6, 7]
+		   // reverse whole array -> [7,6,5,4,3,2,1]
+			//reverse 0, d-1 --> [6, 7, - 5, 4, 3, 2, 1]
+			//reverse d to last --> [6, 7, --, 1, 2, 3, 4,from,  5]
+		public static int[] rotateArray2(int[] arr, int d) {
+
+			// with memory O(m) and compexity O(m)
+			int n = arr.length;
 		
-	     reverseArray(arr, 0, n-1);
-	     reverseArray(arr, n, arr.length -1);
-	     reverseArray(arr, 0, arr.length -1);
-	     return arr;
-	}
-	
-	public static void main(String[] args) {
-		ArrayShift arrayShift = new ArrayShift();
-		System.out.println(Arrays.toString(arrayShift.shiftArray(new int[] {4,6,1,8,7,5}, 3)));
-	}
+			reverseArray(arr, 0, n);
+			reverseArray(arr, 0, d);
+			reverseArray(arr, d, n);
+			return arr;
+		}
+
+		private static void reverseArray(int[] arr, int from, int n) {
+			for(int i =from, j = n-1; i < j; i++, j--) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+	 		}
+		}
+
+		public static void main(String[] args) {
+			System.out.println(Arrays.toString(rotateArray1(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2)));
+			System.out.println(Arrays.toString(rotateArray2(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 2)));
+
+		}
+
 }
